595c3bf3daf35dd5702c38f91040b37f506e1bca
7inh <tranquoclinh@gmail.com>
2024-07-04 11:25:56 +0700

    chore: Update header animation and style

--- lib.js
+++ lib.js
@@ -13,5 +13,14 @@ function loadItemToList() {
             console.log(listItem);
             document.querySelector(".list-item").innerHTML = listItem;
         });
+}
 
+function loadItemToHeader() {
+    fetch("title.txt").then((res) => res.text()).then(data => {
+        const listTitle = data.split("\n") || [];
+        const html = document.getElementById("scrollText").innerHTML;
+        listTitle.forEach((title, index) => {
+            document.getElementById("scrollText").innerHTML += `<div class="scroll-text-item scroll-text${index + 2}">${title}</div>`;
+        })
+    })
 }
